4c929e5294fcd64a62f9675dd66e242040c54273
Jafagervik <jorgenafagervik@gmail.com>
2025-04-02 01:27:47 +0200

    fix certain grievances

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,9 @@
 return {
     "windwp/nvim-autopairs",
-    event = "InsertEnter", -- Load only when entering insert mode
+    event = "InsertEnter",
     opts = {
         disable_filetype = { "TelescopePrompt", "vim" }, -- Skip in these filetypes
-        check_ts = true, -- Use Treesitter for smarter pairing (since you have nvim-treesitter)
+        check_ts = true, -- Use Treesitter for smarter pairing
         -- fast_wrap = { -- Optional: Quick wrap selected text with pairs (e.g., Ctrl-e)
         --     map = "<C-e>",
         --     chars = { "{", "[", "(", '"', "'" },
@@ -12,9 +12,5 @@ return {
     config = function(_, opts)
         local autopairs = require("nvim-autopairs")
         autopairs.setup(opts)
-
-        -- Integrate with blink.cmp
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        require("blink.cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
 }
